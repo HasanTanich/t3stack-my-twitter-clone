ed5630fd8ed6fbfded2c0ced41739901de0e2d4d
Hasan Tanich <hasantanich@gmail.com>
2023-05-17 09:16:29 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Create T3 App
+
+This is a [T3 Stack](https://create.t3.gg/) project bootstrapped with `create-t3-app`.
+
+- [Next.js](https://nextjs.org)
+- [NextAuth.js](https://next-auth.js.org)
+- [Prisma](https://prisma.io)
+- [Tailwind CSS](https://tailwindcss.com)
+- [tRPC](https://trpc.io)
+
+## Learn More
+
+This is a twitter clone application made with T3 Stack for learning purposes.
---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+
+  return (
+    <>
+      <Head>
+        <title>My Twitter Clone</title>
+        <meta name="twitter clone made by Hasan Tanich" content="Twitter clone for learning purposes" />
+        <link rel="icon" href="/logo.ico" />
+      </Head>
+    </>
+  );
+};
+
+export default Home;
